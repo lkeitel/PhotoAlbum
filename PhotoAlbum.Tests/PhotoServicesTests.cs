e35d54b96c7116022f255d020e0ba19d670d201d
Louis Keitel <lkeitel@hy-vee.com>
2022-01-12 21:31:35 -0600

    add Client

--- PhotoAlbum.Tests/PhotoServicesTests.cs
+++ PhotoAlbum.Tests/PhotoServicesTests.cs
@@ -1,13 +1,83 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Protected;
+using PhotoAlbum.Models;
 
 namespace PhotoAlbum.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class PhotoServicesTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private PhotoServices _sut;
+        private List<Photos> expectedResults;
+        private Mock<HttpMessageHandler> mockHttpMessageHandler;
+
+        [TestInitialize]
+        public void BeforeEach()
         {
+            expectedResults = new List<Photos>()
+            {
+                new Photos()
+                {
+                    Id = 1,
+                    AlbumId = 1,
+                    URL = "www.test.com",
+                    thumbnailURL = "www.testthumbnail.com",
+                    Title = "photo 1"
+                    
+                },
+                new Photos()
+                {
+                    Id = 2,
+                    AlbumId = 1,
+                    URL = "www.test.com",
+                    thumbnailURL = "www.testthumbnail.com",
+                    Title = "photo 2"
+                },
+                new Photos()
+                {
+                    Id = 3,
+                    AlbumId = 2,
+                    URL = "www.test.com",
+                    thumbnailURL = "www.testthumbnail.com",
+                    Title = "photo 3"
+                }
+            };
+            
+            mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync((HttpRequestMessage request, CancellationToken token) =>
+                {
+                    HttpResponseMessage response = new HttpResponseMessage()
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        Content = new StringContent(JsonSerializer.Serialize(expectedResults))
+                    };
+
+                    return response;
+                });
+            HttpClient client = new HttpClient(mockHttpMessageHandler.Object);
+            _sut = new PhotoServices(client);
+        }
+        
+        [TestMethod]
+        public void GetPhotosShouldGetListOfPhotosFromClient()
+        {
+            var results = _sut.GetPhotos().Result;
+            results.Should().BeEquivalentTo(expectedResults);
         }
     }
 }
--- PhotoAlbum/Services/PhotoServices.cs
+++ PhotoAlbum/Services/PhotoServices.cs
@@ -1,7 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Threading.Tasks;
+using PhotoAlbum.Models;
+
 namespace PhotoAlbum
 {
     public class PhotoServices
     {
-        
+        private HttpClient _client;
+        public PhotoServices()
+        {
+            _client = new HttpClient();
+        }
+
+        public PhotoServices(HttpClient client)
+        {
+            _client = client;
+        }
+        public async Task<List<Photos>> GetPhotos()
+        {
+            var result = await _client.GetAsync("https://jsonplaceholder.typicode.com/photos");
+            var content = result.Content.ReadAsStringAsync().Result;
+            return JsonSerializer.Deserialize<List<Photos>>(content);
+        }
+
     }
 }
